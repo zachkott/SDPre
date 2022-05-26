063eb79b87200a70ff1a1e34353d5f254d0844aa
zachkott <zachkott@gmail.com>
2022-05-26 15:18:26 -0700

    complete objective2lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,9 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    double num1 = 7;
+    double num2 = 9;
+    double num3 = 4;
+    double average = (num1 + num2 + num3)/3;
+    System.out.println(average);
+  }
+}
