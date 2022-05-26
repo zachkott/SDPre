5e652674b9fd1f06295dc1840c75ea5e3b4684c8
zachkott <zachkott@gmail.com>
2022-05-26 15:07:11 -0700

    complete objective2lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,9 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Zachary Kotterer";
+    int age = 28;
+    String state = "North Carolina";
+    System.out.println("Name: " + name + " Age: " + age +
+    " State: " + state);
+    }
+}
