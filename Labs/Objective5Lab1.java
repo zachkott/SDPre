2a0ff671502862def0175a089ec5c4199a3544c1
zachkott <zachkott@gmail.com>
2022-05-27 14:56:54 -0700

    complete objective5lab1

---
+++ Labs/Objective5Lab1.java
@@ -0,0 +1,13 @@
+public class Objective5Lab1 {
+  public static void main(String[] args) {
+    int num1 = 5;
+    int num2 = 12;
+
+    if(num1 > num2) {
+      System.out.println(num1 + " is greater than " + num2 + ".");
+    }
+    if(num2 > num1) {
+      System.out.println(num2 + " is greater than " + num1 + ".");
+    }
+  }
+}
