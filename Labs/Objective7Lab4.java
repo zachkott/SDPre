07d011c4c1c89e5907f783749e9f487d01a846c5
zachkott <zachkott@gmail.com>
2022-05-27 16:35:59 -0700

    complete objective7lab4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,12 @@
+public class Objective7Lab4 {
+  public static void main(String[] args) {
+    int counter = 1;
+    int sum = 0;
+
+    while(counter <= 20) {
+      sum += counter;
+      counter = counter + 1;
+    }
+    System.out.println(sum);
+  }
+}
