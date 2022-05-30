9340259e757862c73cb17ec4645a169673b8d7f8
zachkott <zachkott@gmail.com>
2022-05-30 12:05:29 -0700

    complete objective8lab3

---
+++ Labs/Objective8Lab3.java
@@ -0,0 +1,14 @@
+public class Objective8Lab3 {
+  public static void main(String[] args) {
+    int counter =1;
+
+    for (int i = 1; i <= 20; i++) {
+      if (i % 2 == 0) {
+        System.out.println(i + " is even");
+      }
+      else {
+        System.out.println(i + " is odd");
+      }
+    }
+  }
+}
