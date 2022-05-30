f41ff28cf82c66b73b44a91502de2fbfb6372e92
zachkott <zachkott@gmail.com>
2022-05-30 11:52:58 -0700

    complete objective8lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,10 @@
+public class Objective8Lab1 {
+  public static void main(String[] args) {
+    for (int i = 1; i <= 10; i++) {
+      System.out.println(i);
+    }
+    for (int i =  10; i >= 1; i--) {
+      System.out.println(i);
+    }
+  }
+}
